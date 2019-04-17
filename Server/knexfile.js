5c58caf4acf859d18780d6b43606fc7272aeb17e
Raz S <razvan.sintimbrean@gmail.com>
2019-04-17 13:43:39 +0300

    db config

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -1,9 +1,22 @@
+// Update with your config settings.
+
 module.exports = {
-  client: "sqlite3",
-  connection: {
-    filename: "./dev.sqlite3"
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./db/dev.sqlite3"
+    },
+    migrations: {
+      directory: "./db/migrations"
+    }
   },
-  migrations: {
-    tableName: "knex_migrations"
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: "./db/test.sqlite3"
+    },
+    migrations: {
+      directory: "./db/migrations"
+    }
   }
 };
--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,25 +1,40 @@
 const express = require("express");
-const router = express.Router();
-const knexfile = require("../knexfile");
-const knex = require("knex")(knexfile);
-const joi = require("joi");
 
-router.get("/", function(req, res) {
+const router = express.Router();
+const joi = require("joi");
+const knex = require("../db/config");
+
+router.get("/", async (req, res) => {
   res.send({ message: "Hello. You are @ localhost:3000/api" });
 });
 
-router.post("/", async (req, res, next) => {
-  const schema = joi.object().keys({
+router.post("/", async (req, res) => {
+  const memberSchema = joi.object().keys({
     user: joi.string()
   });
 
+  const createRoom = async () => {
+    let roomId = Math.ceil(Math.random() * 100);
+    try {
+      await knex("rooms").insert({ uid: roomId });
+      return roomId;
+    } catch (error) {
+      error.errno === 19 ? await createRoom() : res.sendStatus(500);
+    }
+  };
+  const roomId = await createRoom();
+
   try {
-    await joi.validate(req.body, schema);
+    await joi.validate(req.body, memberSchema);
     await knex("members").insert({ name: req.body.user });
     res.sendStatus(200);
   } catch (error) {
-    res.sendStatus(400);
+    error.errno === 19
+      ? res.status(400).send("Name already taken")
+      : res.sendStatus(400);
   }
+
+  res.send({ roomId });
 });
 
 module.exports = router;
