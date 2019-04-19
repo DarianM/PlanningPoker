290e8d6c2ac36687ed13d19ee60cce5ebbe8fbfe
Avasan Darian <darian410@gmail.com>
2019-04-19 12:36:44 +0300

    Adds a test for api/room route

--- Server/test/apiTest.test.js
+++ Server/test/apiTest.test.js
@@ -1,12 +1,28 @@
+process.env.NODE_ENV = "test";
 const request = require("supertest");
 const app = require("../app");
+const knex = require("../db/config");
 
-describe("Test the root path", () => {
-  it("Hello API Request", async () => {
-    const result = await request(app).get("/api");
-    expect(result.body).toEqual({
-      message: "Hello. You are @ localhost:3000/api"
+describe("Routes: Room", () => {
+  beforeAll(() => {
+    return knex.migrate.latest();
+  });
+
+  afterAll(() => {
+    return knex.migrate.rollback();
+  });
+
+  describe("/api/room route", () => {
+    describe("when creating a new room", () => {
+      let result;
+      beforeEach(async () => {
+        result = await request(app).post("/api");
+      });
+
+      it("should return 200", () => expect(result.statusCode).toBe(200));
+
+      it("should return the new room id", () =>
+        expect(typeof result.body.roomId).toBe("number"));
     });
-    expect(result.statusCode).toEqual(200);
   });
 });
