faddfc219cb689bec4eeef0ae482e50db0b3d142
Alin Irimias <alin.irimias@juniormind.com>
2019-05-13 20:21:40 +0300

    Moves the client socket ping to connectedSocket
    Removes multiple pings registration and handles server crash
    when a client has been disconnected

--- Server/connectedSocket.js
+++ Server/connectedSocket.js
@@ -1,43 +1,64 @@
+const PING_INTERVAL = 7000;
+
 module.exports = class connectedSocket {
   constructor(socket, roomId, server) {
     this.socket = socket;
     this.server = server;
     this.roomId = roomId;
     this.isAlive = true;
+
     socket.on("pong", () => this.pong());
     socket.on("message", message => {
-      typeof message === "string" ? this.noop() : this.onmessage(message);
+      if (message === "string") 
+        this.onmessage(message);
     });
     socket.on("close", () => this.onclose());
     socket.on("error", err => console.log(err));
+
+    this.schedulePing();
   }
 
-  noop() {}
   onmessage(message) {
     this.broadcastMessage(JSON.parse(message));
+  }
+ 
+  terminate() {
+    this.isAlive = false;
+    this.clearPing();
+    this.socket.terminate();
+    this.server.disconnect(this.socket, this.roomId);
+  }
+
+  onclose() {
+    this.terminate();
+  }
+
+  schedulePing() {
+    this.interval = setTimeout(() => this.ping(), PING_INTERVAL);
+  }
+
+  clearPing() {
+    clearInterval(this.interval);
+  }
+
+  send(payload) {
+    if (this.socket.readyState === 1)
+      this.socket.send(payload);
   }
 
   ping() {
     if (!this.isAlive) {
-      this.socket.terminate();
+      this.terminate();
+      return;
     }
     this.isAlive = false;
     this.socket.ping("ping");
   }
-  terminate() {
-    this.socket.terminate();
-  }
 
-  onclose() {
-    this.server._roomsSockets[this.roomId] = this.server._roomsSockets[
-      this.roomId
-    ].filter(socket => socket !== this.socket);
-    this.terminate();
-    clearInterval(this.server.interval);
-  }
   pong() {
     this.isAlive = true;
   }
+
   broadcastMessage(message) {
     const { action, data } = message;
     switch (action) {
--- Server/wsServer.js
+++ Server/wsServer.js
@@ -5,34 +5,29 @@ module.exports = class wsServer {
     this._server = server;
     this._roomsSockets = {};
     this.clients = [];
-    this.interval = null;
   }
 
   addSocketToRoom(socket, roomId) {
-    this._roomsSockets[roomId]
-      ? this._roomsSockets[roomId].push(socket)
-      : (this._roomsSockets[roomId] = [socket]);
+    this._roomsSockets[roomId] = this._roomsSockets[roomId] || [];
+    this._roomsSockets[roomId].push(socket);
   }
 
   broadcast(roomId, data) {
     this._roomsSockets[roomId] = this._roomsSockets[roomId] || [];
     this._roomsSockets[roomId].forEach(s => {
-      if (s.readyState === 1) s.send(data);
+      s.send(data);
     });
+  }
+
+  disconnect(client, roomId) {
+    this._roomsSockets[roomId] = this._roomsSockets[roomId].filter(socket => socket !== client);
   }
 
   listen() {
     this._server.on("connection", (s, req) => {
       const roomId = req.url.substring(1);
       this.addSocketToRoom(s, roomId);
-      const client = new connectedSocket(s, roomId, this);
-      this.clients.push(client);
-
-      this.interval = setTimeout(() => {
-        this.clients.forEach(c => {
-          c.ping();
-        });
-      }, 7000);
+      this.clients.push(new connectedSocket(s, roomId, this));
     });
   }
 };
