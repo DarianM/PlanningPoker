cd2d979449b4b6e9695354832c60424315fc7d34
Alin Irimias <alin.irimias@juniormind.com>
2019-05-14 09:15:54 +0300

    Clean slate socket middleware

--- Client/src/middleware/websocket.js
+++ Client/src/middleware/websocket.js
@@ -1,16 +1,5 @@
-import { WEBSOCKET_CONNECT, WEBSOCKET_SEND } from "../actions/types";
-import WS from "../wsProps";
-
-let websocket;
-
-const socketMiddleware = store => next => action => {
-  if (action.type === WEBSOCKET_CONNECT) {
-    websocket = new WS(action.payload, store.dispatch);
-  }
-  if (action.type === WEBSOCKET_SEND) {
-    websocket.send(JSON.stringify(action.payload));
-  }
+const websocketMiddleware = store => next => action => {
   return next(action);
 };
 
-export default socketMiddleware;
+export default websocketMiddleware;
