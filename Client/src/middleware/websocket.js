b25641b1ae42216e2728c58a204a446146147079
Alin Irimias <alin.irimias@juniormind.com>
2019-05-14 10:51:42 +0300

    Websocket middleware creates the websocket on WEBSOCKET_CONNECT action

--- Client/src/middleware/websocket.js
+++ Client/src/middleware/websocket.js
@@ -1,4 +1,9 @@
+import { WEBSOCKET_CONNECT } from "../actions/types";
+
+let websocket;
+
 const websocketMiddleware = store => next => action => {
+  websocket = new WebSocket(action.payload);
   return next(action);
 };
 
--- Client/src/middleware/websocket.test.js
+++ Client/src/middleware/websocket.test.js
@@ -1,3 +1,41 @@
-import socketMiddleware from "./websocket";
+import websocketMiddleware from "./websocket";
+import { connect } from "../actions/websocketActions";
 
-describe("websocket", () => {});
+const createWebSocket = () => {
+  const websocket = {
+    onopen: jest.fn(),
+    onerror: jest.fn(),
+    onclose: jest.fn(),
+    onmessage: jest.fn(),
+    send: jest.fn()
+  };
+  const websocketConstructor = jest.fn(() => websocket);
+  return { websocketConstructor, websocket };
+};
+
+const createMiddleware = () => {
+  const store = {
+    dispatch: jest.fn()
+  };
+  const next = jest.fn();
+  const invoke = action => websocketMiddleware(store)(next)(action);
+
+  return { store, next, invoke };
+};
+
+describe("websocket middleware", () => {
+  const { invoke } = createMiddleware();
+  const { websocketConstructor } = createWebSocket();
+
+  global.WebSocket = websocketConstructor;
+
+  describe("on WEBSOCKET_CONNECT action", () => {
+    invoke(connect("roomId"));
+
+    it("creates the websocket", () => {
+      expect(websocketConstructor).toHaveBeenCalledWith(
+        "ws://localhost:2345/roomId"
+      );
+    });
+  });
+});
