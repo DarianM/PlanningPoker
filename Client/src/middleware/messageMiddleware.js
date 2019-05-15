26d7f2719e506ea4be2fab0e3a08a6ad2a55d6e1
Alin Irimias <alin.irimias@juniormind.com>
2019-05-15 14:03:38 +0300

    Moves websocket ping into message middleware

--- Client/src/middleware/messageMiddleware.js
+++ Client/src/middleware/messageMiddleware.js
@@ -1,32 +1,18 @@
 import { addToast } from "../actions/toastsActions";
 import { newMember } from "../actions/roomActions";
+import { send } from "../actions/websocketActions";
 import {
   REJOIN_ROOM,
-  WEBSOCKET_CLOSE,
-  WEBSOCKET_MESSAGE
+  WEBSOCKET_ERROR,
+  WEBSOCKET_MESSAGE,
+  WEBSOCKET_RECONNECTING,
+  WEBSOCKET_RECONNECTED,
+  WEBSOCKET_OPEN
 } from "../actions/types";
 
-async function reconnectRoomF(roomId, interval, dispatch, fetch) {
-  try {
-    const response = await fetch(`/api/${roomId}`);
-    if (response.status === 200) {
-      clearInterval(interval);
-      const data = await response.json();
-      dispatch(addToast({ text: "Reconnecting successful..." }));
-      dispatch({
-        type: REJOIN_ROOM,
-        payload: {
-          roomName: data.roomName,
-          members: data.members
-        }
-      });
-    } else {
-      dispatch(addToast({ text: "Server seems to be offline. Retrying..." }));
-    }
-  } catch (error) {
-    dispatch(addToast({ text: "Reconnecting failed..." }));
-  }
-}
+const TIMEOUT_BETWEEN_PINGS = 5000;
+
+let pingInterval = null;
 
 const messageMiddleware = fetch => store => next => async action => {
   if (action.type === WEBSOCKET_MESSAGE) {
@@ -37,13 +23,25 @@ const messageMiddleware = fetch => store => next => async action => {
     }
   }
 
-  if (action.type === WEBSOCKET_CLOSE) {
-    store.dispatch(
-      addToast({ text: "Connection lost. Attempting to reconnect" })
-    );
+  if (action.type === WEBSOCKET_OPEN) {
+    pingInterval = setInterval(() => {
+      store.dispatch(send("ping"));
+    }, TIMEOUT_BETWEEN_PINGS);
   }
 
-  if (action.type === "WEBSOCKET_REJOIN") {
+  if (action.type === WEBSOCKET_ERROR) {
+    if (pingInterval) {
+      clearInterval(pingInterval);
+      pingInterval = null;
+    }
+    store.dispatch(addToast({ text: "Connection lost" }));
+  }
+
+  if (action.type === WEBSOCKET_RECONNECTING) {
+    store.dispatch(addToast({ text: "Attempting to reconnect" }));
+  }
+
+  if (action.type === WEBSOCKET_RECONNECTED) {
     const roomId = store.getState().gameRoom.id;
     const { dispatch } = store;
     const interval = action.payload;
