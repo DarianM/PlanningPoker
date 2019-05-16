ecc9bccd72f4ad5a647da52329cdc0f916c36caf
Raz S <razvan.sintimbrean@gmail.com>
2019-05-17 00:29:52 +0300

    created a function to handle errors from api

--- Client/src/Api.js
+++ Client/src/Api.js
@@ -8,10 +8,10 @@ function post(url, payload, receivedFetch) {
         body: JSON.stringify(payload)
       });
       if (response.ok) {
-        const { id } = await response.json();
-        resolve(id);
+        const data = await response.json();
+        resolve(data);
       } else {
-        reject(new Error("Server offline"));
+        handleError(response, reject);
       }
     } catch (error) {
       reject(new Error("Check your internet connection"));
@@ -19,9 +19,21 @@ function post(url, payload, receivedFetch) {
   });
 }
 
+const handleError = (response, reject) => {
+  if (response.status === 504) reject(new Error("Server offline"));
+  if (response.status === 400) {
+    response
+      .json()
+      .then(data => reject(data.error))
+      .catch(() => reject(new Error("Bad request")));
+  }
+};
+
 const vote = (user, roomId, voted) =>
   post("/api/vote", { user, roomId, voted });
 
-const join = (user, roomId) => post("/api/join", { user, roomId });
+const join = (user, roomId) => post("/api/member", { user, roomId });
 
-export { vote, join };
+const create = (user, roomName) => post("/api/room", { user, roomName });
+
+export { vote, join, create };
