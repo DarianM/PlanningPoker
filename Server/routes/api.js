037339861f51e510a9fead13f3edbb05b59eb1c4
Raz S <razvan.sintimbrean@gmail.com>
2019-05-17 00:27:33 +0300

    id no longer needed to reach back to client

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -74,7 +74,7 @@ router.get("/:roomId", validateRoomId, async (req, res) => {
   res.send({ members, roomName });
 });
 
-router.post("/vote", validateMember, async (req, res) => {
+router.post("/vote", validateMember, async req => {
   const { user, roomId, voted } = req.body;
   const id = await db.addMemberVote(user, roomId, voted);
   const data = {
@@ -82,7 +82,6 @@ router.post("/vote", validateMember, async (req, res) => {
     data: { user, roomId, voted, id }
   };
   server.broadcast(roomId, data);
-  res.send({ id });
 });
 
 router.post("/member", validateMember, async (req, res) => {
