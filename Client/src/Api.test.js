4209d5bfff89b541bef13db6fb01d1117929c851
Avasan Darian <darian410@gmail.com>
2019-05-18 00:43:26 +0300

    changed throw error Api

---
+++ Client/src/Api.test.js
@@ -0,0 +1,40 @@
+import Api from "./Api";
+
+describe("with server ok", () => {
+  const url = "http:fake.com";
+  const mockFetch = () =>
+    new Promise((resolve, reject) =>
+      resolve({
+        json: () => ({
+          roomName: "randomName",
+          user: "name"
+        }),
+        ok: true
+      })
+    );
+  it("should throw exception", async () => {
+    const result = await Api(url, {}, mockFetch);
+    expect(result).toEqual({ user: "name", roomName: "randomName" });
+  });
+});
+
+describe("with error from server", () => {
+  const url = "http:fake.com";
+  const mockFetch = jest.fn(
+    () =>
+      new Promise((resolve, reject) =>
+        resolve({
+          json: () => ({
+            error: "this from server..."
+          }),
+          status: 400,
+          ok: false
+        })
+      )
+  );
+  it("should throw error message", () => {
+    const result = Api(url, { user: "random", roomName: "room" }, mockFetch);
+    expect(result).rejects.toThrowError();
+    expect(result).rejects.toEqual("this from server...");
+  });
+});
