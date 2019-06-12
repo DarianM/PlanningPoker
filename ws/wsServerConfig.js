68e7be6684b4d49e1e0dfc331d58bb2262388ab2
Raz S <razvan.sintimbrean@gmail.com>
2019-06-12 18:16:44 +0300

    set ws port to environment port

--- ws/wsServerConfig.js
+++ ws/wsServerConfig.js
@@ -1,6 +1,6 @@
 const ws = require("ws").Server;
 
-const wss = new ws({ port: "2345" });
+const wss = new ws({ port: process.env.PORT });
 const wsServer = require("./wsServer");
 
 const server = new wsServer(wss);
