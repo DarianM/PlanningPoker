7622de84e5ad68ef5d861c02af7bac76724b82fd
Raz S <razvan.sintimbrean@gmail.com>
2019-06-17 14:57:07 +0300

    added story modals validation

--- Client/src/components/session/chart.jsx
+++ Client/src/components/session/chart.jsx
@@ -1,22 +1,48 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getActiveStory } from "../../selectors";
 
-const Chart = () => (
+const mapStateToProps = state => {
+  const { text: story, id } = getActiveStory(state);
+  const players = state.gameRoom.members.length;
+  const { votes } = state.stories.byId[id];
+  const average =
+    votes.reduce((acc, curr) => {
+      return curr.vote === "1/2" ? acc + 0.5 : acc + Number(curr.vote);
+    }, 0) / votes.length;
+  return { story, players, average };
+};
+
+const ConnectedChart = ({ story, players, average }) => (
   <div className="chart-container">
-    <div className="chart-title">enter history in props</div>
+    <div className="chart-title">{story}</div>
     <div className="chart-donut">
-      <div className="donut-circle1">2Players</div>
+      <div className="donut-circle1">
+        {players}
+        {players > 1 ? "Players" : "Player"}
+      </div>
       <div className="donut-circle2">voted</div>
-      <div className="donut-circle3">Avg: 40</div>
+      <div className="donut-circle3">Avg: {average}</div>
     </div>
     <div className="chart-votes">
       <ul>
-        <li>vote</li>
-        <span>50%</span>
-        <li>vote</li>
-        <span>50%</span>
+        <li />
+        <span />
+        <li />
+        <span />
       </ul>
     </div>
   </div>
 );
 
-export default Chart;
+ConnectedChart.propTypes = {
+  story: PropTypes.string.isRequired,
+  players: PropTypes.number.isRequired,
+  average: PropTypes.number.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(ConnectedChart);
